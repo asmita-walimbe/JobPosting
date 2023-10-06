5d3d5570bfdf784674bab8ce4eaadcd931354e48
asmita-walimbe <146116578+asmita-walimbe@users.noreply.github.com>
2023-10-06 14:45:28 +0530

    Added unit test cases

---
+++ src/JobPosting.Web.Test/Controller/GetByIdAsyncTests.cs
@@ -0,0 +1,66 @@
+﻿using FluentValidation;
+using JobPosting.Controllers;
+using JobPosting.Dto;
+using JobPosting.Interface;
+using JobPosting.Web.Test.MockData;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace JobPosting.Web.Test.Controller
+{
+    public class JobPostingControllerTests
+    {
+        private readonly Mock<IJobPostingService> _mockJobPostingService;
+        private readonly Mock<IValidator<JobPostingDto>> _mockValidator;
+        private JobPostingController _controller;
+        public JobPostingControllerTests()
+        {
+            _mockJobPostingService = new Mock<IJobPostingService>();
+            _mockValidator = new Mock<IValidator<JobPostingDto>>();
+            _controller = new JobPostingController(_mockJobPostingService.Object, _mockValidator.Object);
+        }
+
+        [Fact]
+        public async Task GetByIdTest_WithEmptyResponse()
+        {
+            Guid jobPostingId = Guid.NewGuid();
+            //Arrange
+            var mockServiceResponse = _mockJobPostingService.Setup(x => x.GetAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(new JobPostingDto());
+            //Act
+            var response = await _controller.GetById(jobPostingId);
+            //Assert  
+            if (response is ObjectResult objectResult)
+            {
+                var statusCode = objectResult.StatusCode.ToString();
+                statusCode.Equals("200");
+                dynamic data = objectResult.Value;
+                var resultSet = data.Result;
+                resultSet.Equals(new JobPostingDto());
+            }
+        }
+
+        [Fact]
+        public async Task GetByIdTest_WithResponseBody()
+        {
+            var mockData = MockJobPostingDtoModel.GetJobPostingDtoMockData();
+            //Arrange
+            var mockServiceResponse = _mockJobPostingService.Setup(x => x.GetAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(mockData);
+            //Act
+            var response = await _controller.GetById(mockData.Id ?? default);
+            //Assert  
+            if (response is ObjectResult objectResult)
+            {
+                var statusCode = objectResult.StatusCode.ToString();
+                statusCode.Equals("200");
+                dynamic data = objectResult.Value;
+                var resultSet = data.Result;
+                resultSet.Id.Equals(mockData.Id);
+                resultSet.Title.Equals(mockData.Title);
+                resultSet.Description.Equals(mockData.Description);
+            }
+        }
+    }
+}
---
+++ src/JobPosting.Web.Test/MockData/MockJobPostingDtoModel.cs
@@ -0,0 +1,37 @@
+﻿using JobPosting.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobPosting.Web.Test.MockData
+{
+    public static class MockJobPostingDtoModel
+    {
+        public static JobPostingDto GetJobPostingDtoMockData()
+        {
+            return new JobPostingDto()
+            {
+                Id = Guid.NewGuid(),
+                Title = "QA",
+                Description = "Testing",
+            };
+
+        }
+
+        public static List<JobPostingDto> GetAllJobPostingDtoMockData()
+        {
+            return new List<JobPostingDto>()
+            {
+                GetJobPostingDtoMockData(),
+                new JobPostingDto()
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Dev",
+                    Description = "Development",
+                }
+            };
+        }
+    }
+}
